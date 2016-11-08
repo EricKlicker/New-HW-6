540cbd1a554f5b040252badf6a70457c5accfe56
Eric Klicker <ericklicker@yahoo.com>
2016-11-07 21:44:12 -0800

    this better work

---
+++ HW 6 new/HW 6 new/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW 6 new
+//
+//  Created by Eric Klicker on 11/7/16.
+//  Copyright © 2016 Eric Klicker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   
+    return 0;
+}
