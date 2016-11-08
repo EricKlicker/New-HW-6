744ed6eaaa762f390c6ef0c298ac928ac9b1da06
Eric Klicker <ericklicker@yahoo.com>
2016-11-07 21:49:03 -0800

    13.1 and 13.2

--- HW 6 new/HW 6 new/main.cpp
+++ HW 6 new/HW 6 new/main.cpp
@@ -7,8 +7,56 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-   
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    //Excercise 13.1
+    ofstream output;
+    
+    
+    output.open("Exercise13_1.txt");     //create a file
+    srand(time(NULL));
+    
+    int number = rand() % 100;
+    for (int i = 0; i < 100; i++)
+    {
+        output << number << endl;
+        number = rand() % 100;
+    }
+    output.close();
+    
+    cout << "done" << endl;
+    
+    
+    
+    //Excercise 13.2
+    string fileName;
+    char c;
+    int count = -1;
+    cout << "Enter a file name: " << endl;
+    cin >> fileName;
+    
+    ifstream input(fileName.c_str());
+    
+    if (input.fail())
+        cout << fileName << " does not exist" << endl;
+    else
+        cout << fileName << " exists " << endl;
+    
+    while (!input.eof())
+    {
+        input >> c;
+        ++count;
+    }
+    cout << count << endl;
     return 0;
 }
+
